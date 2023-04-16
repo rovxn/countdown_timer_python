ab2ad6af17d86f1d542c44eab5a0d802eeda49f6
Rovn <thomjoh995@gmail.com>
2023-04-16 21:00:14 +0530

    COMMIT MAIN

--- Minor_Project.py
+++ Minor_Project.py
@@ -1,113 +1,97 @@
 import tkinter as tk
-import tkinter.messagebox
-import time
+from tkinter import messagebox
+import threading
 
-
-class Application(tk.Frame): 
-    def __init__(self, master, *args, **kwargs):
-        tk.Frame.__init__(self, master, *args, **kwargs)
+class CountdownTimer:
+    def __init__(self, master):
         self.master = master
-        self.running = False
-        self.time = 0
-        self.hours = 0
-        self.mins = 0
-        self.secs = 0
-        self.build_interface()
+        self.master.title("Countdown Timer")
+        self.time_remaining = 0
+        self.is_paused = False
 
-    def build_interface(self):
-        self.time_entry = tk.Entry(self)
-        self.time_entry.grid(columnspan=2,row=0, column=2,padx=(0,5))
-        
-        self.time_label = tk.Label(self, text="Enter Time (in seconds) :- ", font=("Montserrat", 10), width=25)
-        self.time_label.grid(row=0,columnspan=2, column=0, sticky="N")
+        # Create widgets
+        self.label_time = tk.Label(self.master, text="Time remaining: 0 seconds", font=("Montserrat", 16))
+        self.label_time.pack(pady=10)
 
-        self.clock = tk.Label(self, text="00:00:00", font=("Montserrat", 20), width=10)
-        self.clock.grid(padx=(40,0),row=1, column=1, stick="S")
+        self.frame_input = tk.Frame(self.master)
+        self.frame_input.pack()
 
-        self.time_label = tk.Label(self, text="HOUR   MIN   SEC", font=("Montserrat", 10), width=15)
-        self.time_label.grid(padx=(32,0),row=2, column=1, sticky="N")
+        self.label_input = tk.Label(self.frame_input, text="Enter time (in seconds):",font=("Montserrat", 10))
+        self.label_input.pack(side=tk.LEFT, padx=5)
 
-        self.power_button = tk.Button(self, text="Start", command=lambda: self.start())
-        self.power_button.grid(padx=(5,0),pady=(0,5),row=3, column=0, sticky="NE")
+        self.entry_time = tk.Entry(self.frame_input, width=10)
+        self.entry_time.pack(side=tk.LEFT, padx=5)
 
-        self.reset_button = tk.Button(self, text="Reset", command=lambda: self.reset())
-        self.reset_button.grid(padx=(5,0),pady=(0,5),row=3, column=1, sticky="NW")
+        self.button_start = tk.Button(self.master, text="Start", command=self.start_timer)
+        self.button_start.pack(side=tk.LEFT,pady=10,padx=(40,10))
 
-        self.quit_button = tk.Button(self, text="Quit", command=lambda: self.quit())
-        self.quit_button.grid(padx=(5,5),pady=(0,5),row=3, column=3, sticky="NE")
+        self.button_reset = tk.Button(self.master, text="Reset", command=self.reset_timer)
+        self.button_reset.pack(side=tk.LEFT,pady=5,padx=10)
 
-        self.pause_button = tk.Button(self, text="Pause", command=lambda: self.pause())
-        self.pause_button.grid(pady=(0,5),row = 3,column=2, sticky = "NW")
+        self.button_stop = tk.Button(self.master, text="Stop", command=self.stop_timer)
+        self.button_stop.pack(side=tk.LEFT,pady=5,padx=10)
 
-        self.master.bind("<Return>", lambda x: self.start())
-        self.time_entry.bind("<Key>", lambda v: self.update())
+        self.button_pause_resume = tk.Button(self.master, text="Pause", command=self.pause_resume_timer)
+        self.button_pause_resume.pack(side=tk.LEFT,pady=5,padx=10)
 
-    def calculate(self):
-        self.hours = self.time // 3600
-        self.mins = (self.time // 60) % 60
-        self.secs = self.time % 60
-        return "{:02d}:{:02d}:{:02d}".format(self.hours, self.mins, self.secs)
+    def start_timer(self):
+        if self.time_remaining > 0:
+            messagebox.showwarning("Warning", "Timer is already running!")
+            return
 
-    def update(self):
-        self.time = int(self.time_entry.get())
         try:
-            self.clock.configure(text=self.calculate())
-        except:
-            self.clock.configure(text="00:00:00")
+            self.time_remaining = int(self.entry_time.get())
+        except ValueError:
+            messagebox.showerror("Error", "Invalid input. Please enter an integer.")
+            return
 
-    def timer(self):
-        if self.running:
-            if self.time <= 0:
-                self.clock.configure(text="Time's up!")
-            else:
-                self.clock.configure(text=self.calculate())
-                self.time -= 1
-                self.after(1000, self.timer)
+        if self.time_remaining <= 0:
+            messagebox.showerror("Error", "Invalid input. Please enter a positive integer.")
+            return
 
-    def start(self):
-        try:
-            self.time = int(self.time_entry.get())
-            self.time_entry.delete(0, 'end')
-        except:
-            self.time = self.time
-        self.power_button.configure(text="Stop", command=lambda: self.stop())
-        self.master.bind("<Return>", lambda x: self.stop())
-        self.running = True
-        self.timer()
+        self.update_label()
 
-    def stop(self):
-        self.power_button.configure(text="Start", command=lambda: self.start())
-        self.master.bind("<Return>", lambda x: self.start())
-        self.running = False
+        # Start a new thread to run the countdown
+        self.thread_timer = threading.Thread(target=self.run_timer)
+        self.thread_timer.start()
 
-    def reset(self):
-        self.power_button.configure(text="Start", command=lambda: self.start())
-        self.master.bind("<Return>", lambda x: self.start())
-        self.running = False
-        self.time = 0
-        self.clock["text"] = "00:00:00"
+    def reset_timer(self):
+        self.time_remaining = 0
+        self.is_paused = False
+        self.update_label()
 
-    def quit(self):
-        if tk.messagebox.askokcancel("Quit", "Do you want to quit?"):
-            root.destroy()
+    def stop_timer(self):
+        self.time_remaining = 0
+        self.is_paused = False
+        self.update_label()
+        messagebox.showinfo("Info", "Timer stopped!")
 
-    def pause(self):
-        self.pause_button.configure(text="Resume", command=lambda: self.resume())
-        self.master.bind("<Return>", lambda x: self.resume())
-        if self.running == True:
-            self.running = False
-        self.timer()
-      
+    def pause_resume_timer(self):
+        if self.time_remaining <= 0:
+            messagebox.showwarning("Warning", "Timer is not running!")
+            return
 
-    def resume(self):
-        self.pause_button.configure(text="Pause", command=lambda: self.pause())
-        self.master.bind("<Return>", lambda x: self.pause())
-        if self.running == False:
-            self.running = True
-        self.timer()
+        if self.is_paused:
+            self.is_paused = False
+            self.button_pause_resume.config(text="Pause")
+        else:
+            self.is_paused = True
+            self.button_pause_resume.config(text="Resume")
+
+    def run_timer(self):
+        while self.time_remaining > 0:
+            if not self.is_paused:
+                self.time_remaining -= 1
+                self.update_label()
+            threading.Event().wait(1)
+
+        messagebox.showinfo("Info", "Time's up!")
+        self.reset_timer()
+
+    def update_label(self):
+        self.label_time.config(text="Time remaining: {} seconds".format(self.time_remaining))
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Countdown Timer")
-    Application(root).pack(side="top", fill="both", expand=True)
+    app = CountdownTimer(root)
     root.mainloop()
